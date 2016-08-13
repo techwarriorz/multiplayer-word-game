dccc09a44d0e869c20c060da69ec873e18f8683c
Rick Dawson <rick.dawson@azubu.com>
2016-08-13 03:20:38 -0700

    socket.io heroku fix

--- app/home/home-components.js
+++ app/home/home-components.js
@@ -1,7 +1,7 @@
 var Chat = React.createClass({
   getInitialState: function(){
     return {
-      socket: io()
+      socket: io('https://multiplayer-word-game.herokuapp.com/')
     }
   },
   render: function(){
