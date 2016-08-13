fb09ed491a09568ee0ffb20c3a0346b3121188c2
Rick Dawson <rick.dawson@azubu.com>
2016-08-13 02:16:02 -0700

    port fix

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ var express = require('express'),
     app     = express(),
     http    = require('http').Server(app),
     io      = require('socket.io')(http);
-    http.listen(8080, "127.0.0.1");
+    http.listen(process.env.PORT || 5000);
 
 app.use('/app', express.static(__dirname + '/app'));
 app.get('/', function(req, res){
   res.sendFile(__dirname + "/index.html");
 });
 
-app.listen('3000', function(){
+app.listen(process.env.PORT || 5000, function(){
   console.log("this is running");
 });
