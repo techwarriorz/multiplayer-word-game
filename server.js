2059c11b5836a565693c43faf5790e17506e0901
Rick Dawson <rick.dawson@azubu.com>
2016-08-13 03:50:08 -0700

    server testing

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
 var express = require('express'),
     app     = express(),
-    http    = require('http').Server(app),
+    http    = require('http'),
     socketIO      = require('socket.io');
     //http.listen(process.env.PORT || 5000);
 
 const PORT = process.env.PORT || 3000;
 const INDEX = __dirname + '/index.html';
 
-var server = express();
-server.listen(PORT, () => console.log(`Listening on ${ PORT }`));
-server.use('/app', express.static(__dirname + '/app'));
-server.get('/', function(req, res){
-  res.sendFile(__dirname + "/index.html");
-});
-const io = socketIO(server);
-
+var server = http.createServer(app);
+const io = socketIO.listen(server);
+server.listen(PORT);
 
 io.on('connection', (socket)=> {
   console.log("Client Connected");
   socket.on('disconnect', () => console.log("Client Disconnected"));
+});
+
+app.use('/app', express.static(__dirname + '/app'));
+app.get('/', function(req, res){
+  res.sendFile(__dirname + "/index.html");
 });
 
 
