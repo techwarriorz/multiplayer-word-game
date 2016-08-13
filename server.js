9100f58bf69aa5533f34feb1bb046f482664ccda
Rick Dawson <rick.dawson@azubu.com>
2016-08-13 06:33:51 -0700

    Adding Body Parser

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var express = require('express'),
     app     = express(),
     http    = require('http'),
     socketIO      = require('socket.io'),
-    mongoose      = require('mongoose');
+    mongoose      = require('mongoose'),
+    bodyParser    = require('body-parser');
     //http.listen(process.env.PORT || 5000);
 
 const PORT = process.env.PORT || 3000;
@@ -19,6 +20,9 @@ io.on('connection', (socket)=> {
   socket.on('disconnect', () => console.log("Client Disconnected"));
   socket.on('message', (message)=> io.emit('receive-message', message));
 });
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 app.use('/app', express.static(__dirname + '/app'));
 app.get('/', function(req, res){
