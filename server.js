8099cc890cb5630e100e4955c2fd639f4a62029a
Rick Dawson <rick.dawson@azubu.com>
2016-08-13 02:25:03 -0700

    getting this to work

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ var express = require('express'),
     app     = express(),
     http    = require('http').Server(app),
     io      = require('socket.io')(http);
-    http.listen(process.env.PORT || 5000);
+    //http.listen(process.env.PORT || 5000);
 
-app.set('port', (process.env.PORT || 5000));
+//app.set('port', (process.env.PORT || 5000));
 app.use('/app', express.static(__dirname + '/app'));
 app.get('/', function(req, res){
   res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(app.get('port'), function(){
+app.listen(process.env.PORT || 80, function(){
   console.log("this is running");
 });
